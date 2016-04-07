852025f6f42b893ead1cc84c90bb56199ab95cab
ashroyer <ashroyer@gmail.com>
2016-04-07 15:36:14 -0400

    drawing something upon key event

--- src/window-loaded.js
+++ src/window-loaded.js
@@ -1,28 +1,27 @@
-"use strict";
+'use strict';
 var rsz=()=>{
     document.getElementById('c').width=document.body.clientWidth;
     document.getElementById('c').height=document.body.clientHeight;
-    draw();
+    draw(txt);
 };
 
-var txt="hi, peeps!";
-var draw=()=>{
+var txt='hi, peeps!';
+var draw=(txt)=>{
     var c=document.getElementById('c').getContext('2d');
-    c.fillStyle='rgba(20,40,80,0.1)';
+    c.fillStyle='rgba(255,255,255,0.99)';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='rgba(20,100,131,0.7)';
     c.font='38px monospace';
     c.strokeText(txt,50,50);
 };
 
-window.onload=()=>{
+onload=()=>{
+    var keys=[];
     // initialization; event listeners
-    window.onresize=rsz;
-    window.addEventListener('keydown',k=>{
-        if(k.defaultPrevented){return;}
-        console.log(k);
-        console.log(k.keyIdentifier);
-        k.preventDefault();
-    },true);
+    onresize=rsz;
+    onkeydown=onkeyup=(k)=>{
+        keys[k.keyCode]=k.type=='keydown';
+        draw(keys.reduce((a,b)=>a+b));
+    };
     rsz();
 };
