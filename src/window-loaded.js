2296dba480646cd18bb9b27f57c423d4f8a5b8c7
ashroyer <ashroyer@gmail.com>
2016-04-07 11:57:11 -0400

    derp remove old js file

--- src/window-loaded.js
+++ src/window-loaded.js
@@ -1,24 +1,13 @@
-rsz=_=>{
-    d=document;
-    d.getElementById('c').width=d.body.clientWidth;
-    d.getElementById('c').height=d.body.clientHeight;
+"use strict";
+var rsz=()=>{
+    document.getElementById('c').width=document.body.clientWidth;
+    document.getElementById('c').height=document.body.clientHeight;
     draw();
 };
 
-init=_=>{
-    window.addEventListener('keydown',k=>{
-        if(k.defaultPrevented)return;
-        console.log(k);
-        console.log(k.keyIdentifier);
-        k.preventDefault();
-    },true);
-    rsz();
-};
-
-txt="Ahoy, mates!";
-
-draw=_=>{
-    c=document.getElementById('c').getContext('2d');
+var txt="hi, peeps!";
+var draw=()=>{
+    var c=document.getElementById('c').getContext('2d');
     c.fillStyle='rgba(20,40,80,0.1)';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='rgba(20,100,131,0.7)';
@@ -26,6 +15,14 @@ draw=_=>{
     c.strokeText(txt,50,50);
 };
 
-window.onresize=rsz;
-window.onload=init;
-
+window.onload=()=>{
+    // initialization; event listeners
+    window.onresize=rsz;
+    window.addEventListener('keydown',k=>{
+        if(k.defaultPrevented){return;}
+        console.log(k);
+        console.log(k.keyIdentifier);
+        k.preventDefault();
+    },true);
+    rsz();
+};
