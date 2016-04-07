81d16a50179153f375ba4e4daf1986a2c18b922f
ashroyer <ashroyer@gmail.com>
2016-04-07 10:25:25 -0400

    hi world from canvas

--- src/go.js
+++ src/go.js
@@ -1,10 +1,11 @@
-log=()=>{
-    var x=document.getElementById('c').getContext('2d');
-    x.canvas.width=400;
-    x.canvas.height=100;
-    x.fillStyle='rgba(20,40,80,0.8)';
-    x.fillRect(0,0,x.canvas.width,x.canvas.height);
-    console.log('hi');
+window.onload=()=>{
+    var c=document.getElementById('c').getContext('2d');
+    c.canvas.width=400;
+    c.canvas.height=100;
+    c.fillStyle='rgba(20,40,80,0.1)';
+    c.fillRect(0,0,c.canvas.width,c.canvas.height);
+    c.fillStyle='rgba(20,100,131,0.7)';
+    c.font='38px monospace';
+    c.strokeText('Hello, World!',50,50);
 };
 
-window.onload=log;
