bc98fb04b418d29388e87e2b2bc007e2084b2d23
ashroyer <ashroyer@gmail.com>
2016-04-07 11:40:12 -0400

    key listening

--- src/go.js
+++ src/go.js
@@ -1,21 +1,31 @@
-input=_=>{
-    console.log('hi from the console');
-};
 rsz=_=>{
-    var d=document;
+    d=document;
     d.getElementById('c').width=d.body.clientWidth;
     d.getElementById('c').height=d.body.clientHeight;
     draw();
 };
+
+init=_=>{
+    window.addEventListener('keydown',k=>{
+        if(k.defaultPrevented)return;
+        console.log(k);
+        console.log(k.keyIdentifier);
+        k.preventDefault();
+    },true);
+    rsz();
+};
+
+txt="Ahoy, mates!";
+
 draw=_=>{
     c=document.getElementById('c').getContext('2d');
     c.fillStyle='rgba(20,40,80,0.1)';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='rgba(20,100,131,0.7)';
     c.font='38px monospace';
-    c.strokeText('Hello, World!',50,50);
-    input();
+    c.strokeText(txt,50,50);
 };
+
 window.onresize=rsz;
-window.onload=rsz;
+window.onload=init;
 
