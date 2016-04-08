319c7b3d9499d752bffa305ca603db924fd93366
ashroyer <ashroyer@gmail.com>
2016-04-08 16:55:42 -0400

    sending keycodes, now need to actually get intended characters

--- src/window-loaded.js
+++ src/window-loaded.js
@@ -3,28 +3,34 @@ var rsz=()=>{
     var c=document.getElementById('c').getContext('2d');
     c.canvas.width=window.innerWidth;
     c.canvas.height=window.innerHeight;
-    draw('howdy fsdsdf');
+    print('howdy fsdsdf');
 };
 
-var draw=(txt)=>{
+var decode=(k)=>{
+    // given a Set of KeyboardEvent, get the typed character.
+};
+
+var cursor={x:50,y:75};
+var print=(ks)=>{
     var c=document.getElementById('c').getContext('2d');
     c.fillStyle='rgba(200,200,200,0.99)'; // cheap fade out effect
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='rgba(20,100,131,0.7)';
-    c.font='38px monospace';
-    c.fillText(txt,50,50);
+    c.font='28px monospace';
+    c.fillText(ks,cursor.x,cursor.y);
 };
 
 window.onload=()=>{
     var keyset=new Set();
-    //var keys=[];
     window.onresize=rsz;
     window.onkeydown=window.onkeyup=(k)=>{
-        if(k.type=='keydown')keyset.add(k.code);
-        if(k.type=='keyup')keyset.delete(k.code);
-        //keys[k.keyCode]=k.type=='keydown';
-        draw(k.code+' '+k.type);
-        console.log(keyset);
+        if(k.type=='keydown'){
+            var kc=k.keyCode;
+            console.log(kc);
+            keyset.add(k);
+            print(decode(k));
+        }
+        if(k.type=='keyup')keyset.delete(k);
     };
     rsz();
 };
