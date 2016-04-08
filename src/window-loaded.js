4cf6de5182a2bf73238cb5db6497e3c1616efb2a
ashroyer <ashroyer@gmail.com>
2016-04-08 10:31:34 -0400

    more htmlish

--- src/window-loaded.js
+++ src/window-loaded.js
@@ -1,13 +1,14 @@
 'use strict';
 var rsz=()=>{
-    document.getElementById('c').width=document.body.clientWidth;
-    document.getElementById('c').height=document.body.clientHeight;
+    var c=document.getElementById('c').getContext('2d');
+    c.canvas.width=window.innerWidth;
+    c.canvas.height=window.innerHeight;
     draw('hi world');
 };
 
 var draw=(txt)=>{
     var c=document.getElementById('c').getContext('2d');
-    c.fillStyle='rgba(200,200,200,0.5)'; // cheap fade out effect
+    c.fillStyle='rgba(200,200,200,0.99)'; // cheap fade out effect
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='rgba(20,100,131,0.7)';
     c.font='38px monospace';
@@ -16,7 +17,6 @@ var draw=(txt)=>{
 
 window.onload=()=>{
     var keys=[];
-    // initialization; event listeners
     window.onresize=rsz;
     window.onkeydown=window.onkeyup=(k)=>{
         keys[k.keyCode]=k.type=='keydown';
