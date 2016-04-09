ac51ab02e7628c24d2d88d2d686ca40d1882a293
hoosierEE <ashroyer@gmail.com>
2016-04-09 15:49:59 -0400

    woo letters, numbers, and some symbols

--- src/window-loaded.js
+++ src/window-loaded.js
@@ -3,36 +3,49 @@ var rsz=()=>{
     var c=document.getElementById('c').getContext('2d');
     c.canvas.width=window.innerWidth;
     c.canvas.height=window.innerHeight;
-    write('howdy fsdsdf');
+    reset();
 };
 
-var cursor={x:50,y:75};
-var write=(ks)=>{
-    var c=document.getElementById('c').getContext('2d');
-    //c.fillStyle='rgba(120,80,80,0.99)'; // cheap fade out effect
+const grid={width:25,height:50}; // monospace width/height
+var c=document.getElementById('c').getContext('2d');
+var cursor={x:25,y:75};
+var reset=()=>{
+    cursor.x=grid.width;
+    cursor.y=grid.height;
     c.fillStyle='black';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
+};
+
+var write=(ks)=>{
+    if(undefined===ks)return;
     c.fillStyle='lightGray';
     c.font='28px monospace';
     c.fillText(ks,cursor.x,cursor.y);
+    cursor.x+=grid.width;
+    if(cursor.x>c.canvas.width){cursor.y+=grid.height; cursor.x=grid.width;} // wrap
+};
+
+var decode=({mods:a,key:k})=>{
+    // TODO support modifier keys besides just Shift
+    console.log(k);
+    var ma=k[k.length-1]; // maybe alphanumeric
+    if(k.startsWith('Key')){return a[3]?ma:ma.toLowerCase();}
+    if(k=='Space'){return ' ';}
+    var symbol=")!@#$%^&*(";
+    if(k.startsWith('Digit')){return a[3]?symbol[ma]:ma;}
 };
 
 window.onload=()=>{
     var pressed_codes=new Set();
     window.onresize=rsz;
     window.onkeydown=window.onkeyup=(k)=>{
-        var mod='0123456789abcdef'[[k.altKey,k.ctrlKey,k.metaKey,k.shiftKey]
-                                   .map((a,b)=>a?1<<b:0)
-                                   .reduce((a,b)=>a+b)];
         if(k.type=='keydown'){
-            k.preventDefault();
+            k.preventDefault(); // NOTE: required by browser version, not Chrome app
             pressed_codes.add(k.keyCode);
-            write(mod + String.fromCharCode(k.keyCode));
+            //console.log(k.code);
+            write(decode({mods:[k.altKey,k.ctrlKey,k.metaKey,k.shiftKey],key:k.code}));
         }
-        if(k.type=='keyup'){
-            pressed_codes.delete(k.keyCode);
-        }
-        console.log(mod);
+        if(k.type=='keyup'){pressed_codes.delete(k.keyCode);}
     };
     rsz();
 };
