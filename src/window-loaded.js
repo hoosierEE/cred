55a364fd24cfce77820ba106cf324f0b76d8a0bb
hoosierEE <ashroyer@gmail.com>
2016-04-08 22:51:28 -0400

    modifier keys

--- src/window-loaded.js
+++ src/window-loaded.js
@@ -3,34 +3,36 @@ var rsz=()=>{
     var c=document.getElementById('c').getContext('2d');
     c.canvas.width=window.innerWidth;
     c.canvas.height=window.innerHeight;
-    print('howdy fsdsdf');
-};
-
-var decode=(k)=>{
-    // given a Set of KeyboardEvent, get the typed character.
+    write('howdy fsdsdf');
 };
 
 var cursor={x:50,y:75};
-var print=(ks)=>{
+var write=(ks)=>{
     var c=document.getElementById('c').getContext('2d');
-    c.fillStyle='rgba(200,200,200,0.99)'; // cheap fade out effect
+    //c.fillStyle='rgba(120,80,80,0.99)'; // cheap fade out effect
+    c.fillStyle='black';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
-    c.fillStyle='rgba(20,100,131,0.7)';
+    c.fillStyle='lightGray';
     c.font='28px monospace';
     c.fillText(ks,cursor.x,cursor.y);
 };
 
 window.onload=()=>{
-    var keyset=new Set();
+    var pressed_codes=new Set();
     window.onresize=rsz;
     window.onkeydown=window.onkeyup=(k)=>{
+        var mod='0123456789abcdef'[[k.altKey,k.ctrlKey,k.metaKey,k.shiftKey]
+                                   .map((a,b)=>a?1<<b:0)
+                                   .reduce((a,b)=>a+b)];
         if(k.type=='keydown'){
-            var kc=k.keyCode;
-            console.log(kc);
-            keyset.add(k);
-            print(decode(k));
+            k.preventDefault();
+            pressed_codes.add(k.keyCode);
+            write(mod + String.fromCharCode(k.keyCode));
         }
-        if(k.type=='keyup')keyset.delete(k);
+        if(k.type=='keyup'){
+            pressed_codes.delete(k.keyCode);
+        }
+        console.log(mod);
     };
     rsz();
 };
