ca0ef5de7cb54ff3941131fb17c6572bc227bb53
hoosierEE <ashroyer@gmail.com>
2016-04-11 08:21:18 -0400

    unfinished split of cursor with render

--- js/cred.js
+++ js/cred.js
@@ -4,6 +4,7 @@ var c=document.getElementById('c').getContext('2d'),
     grid={width:25,height:50}, // monospace grid width/height
     cursor={x:25,y:75}, // cursor position on grid
     txt=[],
+    decoded_keys=[],
     pressed_codes=new Set(); // keys that are down right now
 
 var draw_to_canvas=(ks)=>{
--- js/global-vars.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var c=document.getElementById('c').getContext('2d'),
-    grid={width:25,height:50}, // monospace grid width/height
-    cursor={x:25,y:75}, // cursor position on grid
-    pressed_codes=new Set(); // keys that are down right now
