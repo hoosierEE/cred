f1ad3350fe5dba96d5f0ca8fb578bd6aeafde8bb
ashroyer <ashroyer@gmail.com>
2016-04-12 17:23:16 -0400

    ugh cursors

--- cred.js
+++ cred.js
@@ -1,60 +1,63 @@
 'use strict';
 var c=document.getElementById('c').getContext('2d'),
-    grid=25, // monospace grid width/height
-    Cursor=function(){
-        return {
-            x:grid,y:grid*2.5,
-            width:function(){return c.measureText('a').width;},
-            up:function(){this.y-=grid*1.5;},
-            down:function(){this.y+=grid*1.5;},
-            right:function(){this.x+=this.width();},
-            left:function(){this.x-=this.width();}
-        };
-    },
     mode={insert:true,normal:false,visual:false},
-    txt=[''],
-    tw=3,
-    pressed_codes=new Set(); // keys that are down right now
-var cursor=new Cursor(); // for writing text
-var point=new Cursor();
+    grid=25, // monospace grid width/height
+    Cursor=()=>{
+        var crsr={
+            x:0,y:0,
+            width(){return c.measureText('a').width;},
+            init(){this.x=grid;this.y=grid*2.5;},
+            up(){this.y-=grid*1.5;},
+            down(){this.y+=grid*1.5;},
+            right(){this.x+=this.width();
+                    if(this.x>c.canvas.width){this.crlf();}},
+            left(){this.x-=this.width();if(this.x<grid){this.x=grid;}},
+            crlf(){this.x=grid;this.y+=grid*1.5;}
+        };
+        crsr.init();
+        return crsr;
+    },
+    txt={pos:0,data:[]}; // editing position, entire contents
+var cursor=Cursor(); // for drawing text to the screen
+var point=Cursor(); // for the current cursor position
 
 var render_text=()=>{
     requestAnimationFrame(render_text);
-    cursor.x=grid;cursor.y=grid*2.5; // FIXME this resets cursor position to home each time
+    cursor.init();
     c.fillStyle='black';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='lightGray';
-    c.font='28px monospace';
-    var newline=()=>{cursor.x=grid;cursor.y+=grid*1.5;}
-    for(var i=0;i<txt.length;++i){
-        tw=cursor.width();
-        if(cursor.x+tw+grid>c.canvas.width){newline();}
-        if(txt[i]=='\t'){
-            if(cursor.x+tw*4+grid>c.canvas.width){newline();}
+    c.font='28px monospace'; // FIXME non-monospace fonts are messed up
+    var tw=cursor.width();
+    for(var i=0;i<txt.data.length;++i){
+        // TODO only draw what's visible
+        if(cursor.x+tw+grid>c.canvas.width){cursor.crlf();}
+        if(txt.data[i]=='\t'){
+            if(cursor.x+tw*4+grid>c.canvas.width){cursor.crlf();}
             else{cursor.x+=4*tw;}
         }
-        else if(txt[i]=='\n'){newline();}
-        else if(cursor.x+tw+grid>c.canvas.width){newline();}
+        else if(txt.data[i]=='\n'){cursor.crlf();}
+        else if(cursor.x+tw+grid>c.canvas.width){cursor.crlf();}
         else{
             cursor.x+=tw;
-            c.fillText(txt[i],cursor.x,cursor.y);
+            c.fillText(txt.data[i],cursor.x,cursor.y);
         }
     }
-    // cursor
-    this.prev_blink_alpha=0;
     var blink_alpha=0.7+0.5*Math.cos(Date.now()*0.007);
     c.fillStyle='rgba(255,255,255,'+blink_alpha+')';
-    c.fillRect(point.x+point.width(),point.y-grid*1.25,2,grid*1.5);
+    c.fillRect(point.x+point.width(),point.y-grid*1.25,1,grid*1.25);
 };
 
-var decode=(a,c,m,s,k)=>{
-    var action={act:'',c:''};
-    if(k=='Space'){action.c=' ';}
+var decode=(alt,ctrl,meta,shft,k)=>{
+    var decoded={act:'',c:''}; // {action, printable}
+    // printable?
+    if(k=='Space'){decoded.c=' ';}
     var ma=k[k.length-1]; // maybe alphanumeric
     switch(k.slice(0,-1)){
-    case'Key':action.c=s?ma:ma.toLowerCase();break;
-    case'Digit':action.c=s?")!@#$%^&*("[ma]:ma;break;
+    case'Key':decoded.c=shft?ma:ma.toLowerCase();break;
+    case'Digit':decoded.c=shft?")!@#$%^&*("[ma]:ma;break;
     }
+    // other punctuation not associated with an alphanumeric key
     var punct=['Comma',',','<'
                ,'Quote',"'",'"'
                ,'Equal','=','+'
@@ -66,46 +69,59 @@ var decode=(a,c,m,s,k)=>{
                ,'Backquote','`','~'
                ,'BracketLeft','[','{'
                ,'BracketRight',']','}'
-              ]; // maybe all the keys should be done this way, hmm...
+              ];
     var pin=punct.indexOf(k);
-    if(pin>=0){action.c=punct[pin+(s?2:1)];}
+    if(pin>=0){decoded.c=punct[pin+(shft?2:1)];}
 
-    //console.log(k);
-    // TODO #6 cursor movers
+    // weird-shaped characters
     switch(k){
-    case'Tab':action.c='\t';break;
-    case'Enter':action.c='\n';break;
-    case'ArrowLeft':point.left();break;
-    case'ArrowRight':point.right();break;
-    case'ArrowUp':point.up();break;
-    case'ArrowDown':point.down();break;
+    case'Tab':decoded.c='\t';break;
+    case'Enter':decoded.c='\n';break;
     }
+
+    // handle whatever {Action, printable} we just decoded
     var update_txt=(ks)=>{
-        if(!ks){return;}
-        if(ks.act=='Backspace'){txt.pop();}
-        else if(ks.c.length==0){return;}
-        else{txt.push(ks.c);}
+        switch(ks.act){
+        case'Backspace':
+            txt.pos=txt.pos>0?txt.pos-1:0;
+            txt.data.pop();
+            break;
+        case'Delete':txt.data.splice(txt.pos,1);
+        }
+        if(ks.c.length>0){
+            txt.pos+=1;
+            txt.data.push(ks.c);
+        }
     };
+
     var update_point=(ks)=>{
-        if(ks.act.slice(0,5)!='Arrow'){
-            point.x=cursor.x+point.width();
+
+        if(ks.c.length>0){
+            point.x=cursor.x+cursor.width();
             point.y=cursor.y;
         }
-        if(ks.act=='Enter'){point.x=grid;point.down();}
-        //if(ks.act=='Backspace'){point.x=cursor.x-point.width();}
-        if(ks.act=='Backspace'){point.left();point.left();} // FIXME this ugly hack
-        if(ks.act=='Delete'){point.right();}
+        if(ks.act.slice(0,5)=='Arrow'){
+            switch(ks.act.slice(5)){
+            case'Right':point.right();break;
+            case'Left':point.left();break;
+            case'Up':point.up();break;
+            case'Down':point.down();break;
+            }
+        }
+        switch(ks.act){
+        case'Enter':point.crlf();break;
+        }
     };
-    action.act=k; // catch-all
-    update_txt(action);
-    update_point(action);
+    decoded.act=k; // catch-all
+    update_txt(decoded);
+    update_point(decoded);
 };
 
 window.onload=()=>{
     var rsz=()=>{
         c.canvas.width=c.canvas.clientWidth;
         c.canvas.height=c.canvas.clientHeight;
-        cursor.x=grid; cursor.y=grid*2.5;
+        cursor.x=grid; cursor.y=grid*1.5;
         c.fillStyle='black';
         c.fillRect(0,0,c.canvas.width,c.canvas.height);
     };
@@ -113,11 +129,10 @@ window.onload=()=>{
     window.onresize=rsz;
     window.onkeydown=window.onkeyup=(k)=>{
         if(k.type=='keydown'){
+            //if(k.defaultPrevented){return;}
             k.preventDefault();
-            pressed_codes.add(k.keyCode);
             decode(k.altKey,k.ctrlKey,k.metaKey,k.shiftKey,k.code);
         }
-        if(k.type=='keyup'){pressed_codes.delete(k.keyCode);}
     };
     requestAnimationFrame(render_text);
 };
