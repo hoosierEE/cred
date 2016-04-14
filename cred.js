4e3d1e2bb7e0ac9f9306bee94d6c8f0392ecbaa1
hoosierEE <ashroyer@gmail.com>
2016-04-14 08:47:20 -0400

    if-else instead of switch

--- cred.js
+++ cred.js
@@ -64,7 +64,7 @@ var update=(dec_k,state)=>{
     console.log(dec_k);
     switch(dec_k.type){
     case'print':txt.append(dec_k.code);break; // add char to text buffer
-    case'edit':if(dec_k.code=='b'){txt.dec();}break;
+    case'edit':if(dec_k.code=='B'){txt.dec();}break;
     case'arrow':break;
     case'page':break;
     }
@@ -72,34 +72,33 @@ var update=(dec_k,state)=>{
 
 //  decode : [bool] -> string -> {type:string,code:char,mods:[bool]}
 var decode=(mods,k)=>{
-    var dec={type:'',code:'',mods:mods}; // return type
+    var dec={type:'',code:'',mods:mods}; // return type (modifiers pass through)
     // printable
     if(k=='Space'){dec.code=' ';}
     else{
         var shft=mods[3];
         var ma=k.slice(-1); // maybe alphanumeric
-        switch(k.slice(0,-1)){
-        case'Key':dec.code=shft?ma:ma.toLowerCase();break;
-        case'Digit':dec.code=shft?")!@#$%^&*("[ma]:ma;break; // sigh
+        var kd=k.slice(0,-1);
+        if(kd=='Key'){dec.code=shft?ma:ma.toLowerCase();}
+        else if(kd=='Digit'){dec.code=shft?")!@#$%^&*("[ma]:ma;}
+        else if(k=='Tab'){dec.code='\t';}
+        else if(k=='Enter'){dec.code='\n';}
+        else{
+            var pun=['Comma',',','<','Quote',"'",'"','Equal','=','+','Minus','-','_'
+                     ,'Slash','/','?','Period','.','>','Semicolon',';',':','Backslash','\\','|'
+                     ,'Backquote','`','~','BracketLeft','[','{','BracketRight',']','}'];
+            var pid=pun.indexOf(k);
+            if(pid>=0){dec.code=pun[pid+(shft?2:1)];}
         }
-        switch(k){
-        case'Tab':dec.code='\t';break;
-        case'Enter':dec.code='\n';break;
-        }
-        var pun=['Comma',',','<','Quote',"'",'"','Equal','=','+','Minus','-','_'
-                 ,'Slash','/','?','Period','.','>','Semicolon',';',':','Backslash','\\','|'
-                 ,'Backquote','`','~','BracketLeft','[','{','BracketRight',']','}'];
-        var pid=pun.indexOf(k);
-        if(pid>=0){dec.code=pun[pid+(shft?2:1)];}
     }
     // non-printable
     if(dec.code.length>0){dec.type='print';}
     else{
-        if(k=='Backspace'||k=='Delete'){dec.type='edit'; dec.code=k[0].toLowerCase();} // 'b','d'
+        if(k=='Backspace'||k=='Delete'){dec.type='edit';dec.code=k[0];} // 'b','d'
         else if(k=='Escape'){dec.type='escape';} // '' (should still be an empty string)
-        else if(k.slice(0,5)=='Arrow'){dec.type='arrow'; dec.code=k[5].toLowerCase();} // 'u','d','l','r'
-        else if(k.slice(0,4)=='Page'){dec.type='page'; dec.code=k[4].toLowerCase();} // 'u','d'
-        else if(k=='Home'||k=='End'){dec.type='page'; dec.code=k[0].toLowerCase();} // 'h','e'
+        else if(k.slice(0,5)=='Arrow'){dec.type='arrow';dec.code=k[5];} // 'u','d','l','r'
+        else if(k.slice(0,4)=='Page'){dec.type='page';dec.code=k[4];} // 'u','d'
+        else if(k=='Home'||k=='End'){dec.type='page';dec.code=k[0];} // 'h','e'
     }
     return dec;
 };
