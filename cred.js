29a148004683267c9c5f137651474bba4a7a2cc4
hoosierEE <ashroyer@gmail.com>
2016-04-18 22:39:52 -0400

    better word boundaries

--- cred.js
+++ cred.js
@@ -5,44 +5,52 @@ var c=document.getElementById('c').getContext('2d'),
     KEYQUEUE=[{mods:[false,false,false,false],k:''}], // lightens duties for key event handler
     Buffer=()=>({
         // cursor position
-        pos:0,data:[],changed:false,
-        dec(n=1){this.pos-=n;if(this.pos<0){this.pos=0;}},
-        inc(n=1){this.pos+=n;},
-        ins(ch){ // append ch chars to position pos
+        p:0,a:[],changed:false,
+        dec(n=1){this.p-=n;if(this.p<0){this.p=0;}},
+        inc(n=1){this.p+=n;},
+        ins(ch){ // append ch chars to position p
             this.changed=true;
-            if(this.pos==this.data.length){this.data.push(ch);}
-            else{this.data.splice(this.pos,0,ch);}
+            if(this.p==this.a.length){this.a.push(ch);}
+            else{this.a.splice(this.p,0,ch);}
             this.inc();
         },
         del(n){ // delete n chars before (n<0) or after (n>0) cursor
             if(n==0){return;}
             if(n<0){
-                if(this.pos==this.data.length){for(var i=0;i<-n;++i){this.data.pop();}}
-                else{this.data.splice(this.pos+n,-n);}
+                if(this.p==this.a.length){for(var i=0;i<-n;++i){this.a.pop();}}
+                else{this.a.splice(this.p+n,-n);}
                 this.dec(-n);}
             else{
-                if(this.pos==this.data.length){return;}
-                else{for(var i=1;i<=n;++i){this.data.splice(this.pos,i);}}}
+                if(this.p==this.a.length){return;}
+                else{for(var i=1;i<=n;++i){this.a.splice(this.p,i);}}}
             this.changed=true;},
     }),
 
     Cursor=(buf)=>({
         x:0,y:0,width:0,height:0,moved:false,
         home(){this.width=c.measureText('W').width;this.height=this.width*1.5;this.x=this.width;this.y=this.width*1.5;},
-        up(ch='\n'){var d=buf.data.lastIndexOf(ch,buf.pos-1);this.left(buf.pos-(d<0?0:d));},
-        dn(ch='\n'){var d=buf.data.indexOf(ch,buf.pos+1);this.right(d<0?buf.data.length-buf.pos:d-buf.pos);},
+        up(ch='\n'){var d=buf.a.lastIndexOf(ch,buf.p-1), dd=buf.a.lastIndexOf('\n',buf.p-1);
+                    console.log('d: '+d+', dd:'+dd);
+
+                    if(ch==' '){d=dd<0?d:dd;}
+                    this.left(buf.p-(d<0?0:d));},
+        dn(ch='\n'){var d=buf.a.indexOf(ch,buf.p+1), dd=buf.a.indexOf('\n',buf.p+1);
+                    console.log('pos: '+buf.p+', d: '+d+', dd:'+dd);
+
+                    if(ch==' '){d=Math.min(d,(dd<0?d:dd));}//dd<0?d:dd;}
+                    this.right(d<0?buf.a.length-buf.p:d-buf.p);},
         right(n=1){
             while(n-->0){
-                if(buf.pos==buf.data.length){return;}
+                if(buf.p==buf.a.length){return;}
                 this.moved=true;
-                this.width=c.measureText(buf.data[++buf.pos]).width;
+                this.width=c.measureText(buf.a[++buf.p]).width;
                 this.x+=this.width;
                 if(this.x>c.canvas.width){this.crlf();}}},
         left(n=1){
             while(n-->0){
-                if(buf.pos==0){return;}
+                if(buf.p==0){return;}
                 this.moved=true;
-                this.width=c.measureText(buf.data[--buf.pos]).width;
+                this.width=c.measureText(buf.a[--buf.p]).width;
                 this.x-=this.width;
                 if(this.x<this.width){this.x=this.width;}}},
         crlf(){this.x=this.width;this.y+=this.height;},
@@ -51,7 +59,7 @@ var c=document.getElementById('c').getContext('2d'),
 var cur=Cursor(buf); // for drawing text to the screen
 
 var stwrite=(str)=>{for(var i=0;i<str.length;++i){buf.ins(str[i]);}};
-//stwrite(img); // test data
+//stwrite(img); // test a
 stwrite('Hello world!');
 
 var gameloop=(now,resiz)=>{
@@ -67,15 +75,15 @@ var render_text=(now,cur)=>{
     c.fillStyle='#222';
     c.fillRect(0,0,c.canvas.width,c.canvas.height);
     c.fillStyle='lightgray';
-    for(var i=0;i<buf.data.length;++i){
-        if(buf.pos==i){c.fillRect(cur.x, cur.y-cur.height, 1, cur.height);} // draw cursor
-        var tw=c.measureText(buf.data[i]).width; // non-monospace fix!
+    for(var i=0;i<buf.a.length;++i){
+        if(buf.p==i){c.fillRect(cur.x, cur.y-cur.height, 1, cur.height);} // draw cursor
+        var tw=c.measureText(buf.a[i]).width; // non-monospace fix!
         if(cur.y-cur.height>c.canvas.height){break;} // don't render beyond canvas
-        if(buf.data[i]=='\t'){if(cur.x+tw*4+cur.width>c.canvas.width){cur.crlf();}else{cur.x+=4*tw;}}
-        else if(buf.data[i]=='\n'){cur.crlf();}
-        else{c.fillText(buf.data[i],cur.x,cur.y); cur.x+=tw;}
+        if(buf.a[i]=='\t'){if(cur.x+tw*4+cur.width>c.canvas.width){cur.crlf();}else{cur.x+=4*tw;}}
+        else if(buf.a[i]=='\n'){cur.crlf();}
+        else{c.fillText(buf.a[i],cur.x,cur.y); cur.x+=tw;}
         if(cur.x+tw+cur.width>c.canvas.width){cur.crlf();}}
-    if(buf.pos==buf.data.length){c.fillRect(cur.x, cur.y-cur.height, 1, cur.height);}};
+    if(buf.p==buf.a.length){c.fillRect(cur.x, cur.y-cur.height, 1, cur.height);}};
 
 // udpate : [RawKey] -> BufferAction
 var update=(rks)=>{
@@ -85,9 +93,9 @@ var update=(rks)=>{
             switch(dec.code){
             case'i':MODE='insert';break;
             case'a':MODE='insert';cur.right();break;
-            case'A':MODE='insert';if(buf.data[buf.pos]!=='\n'){cur.dn();}break;
-            case'o':MODE='insert';if(buf.data[buf.pos]!=='\n'){cur.dn();}buf.ins('\n');break;
-            case'O':MODE='insert';cur.up();buf.ins('\n');if(buf.pos==1){cur.left();}break;
+            case'A':MODE='insert';if(buf.a[buf.p]!=='\n'){cur.dn();}break;
+            case'o':MODE='insert';if(buf.a[buf.p]!=='\n'){cur.dn();}buf.ins('\n');break;
+            case'O':MODE='insert';cur.up();buf.ins('\n');if(buf.p==1){cur.left();}break;
             case'b':cur.up(' ');break;
             case'e':cur.dn(' ');break;
             case'h':cur.left();break;
