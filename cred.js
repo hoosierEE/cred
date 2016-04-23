b2b52de027334efac6b274d51a432e30870b7f76
hoosierEE <ashroyer@gmail.com>
2016-04-23 19:33:27 -0400

    cursor handles newlines

--- cred.js
+++ cred.js
@@ -6,31 +6,34 @@ var c=document.getElementById('c').getContext('2d'),// rarely changing bottom ca
     ESC_FD=0,// 'fd' escape sequence
     KEYQ=[{mods:[false,false,false,false],k:''}],// lightens duties for key event handler
     Buffer=()=>({// a string with a cursor
-        p:0,a:'',changed:false,oldp:0,
+        pt:0,// number of chars between start of file and cursor position
+        lin:0,// line containing pt
+        col:0,// column containing pt
+        a:'',// text buffer
+        changed:false,
         change:[0,0],//start,end of last modification
-        load(txt){this.ins(txt);},
+        load(txt){this.a='';this.change=[0,0];this.pt=0;this.ins(txt);},
         lines(){return this.a.split(/\n/g);},// array of all the buffer's lines
         words(){return this.a.split(/\s/g).reduce((a,b)=>a.concat(b),[]);},
         ins(ch){// insert ch chars to right of p
             this.changed=true;
-            if(this.p==this.a.length){this.a=this.a+ch;}
-            else{this.a=this.a.slice(0,this.p)+ch+this.a.slice(this.p);}
-            this.change=[this.p,this.p+ch.length];
+            if(this.pt==this.a.length){this.a=this.a+ch;}
+            else{this.a=this.a.slice(0,this.pt)+ch+this.a.slice(this.pt);}
+            this.change=[this.pt,this.pt+ch.length];
             this.mov(ch.length);
         },
         del(n){// delete n chars to right of p (or left if n<0)
-            if(n==0||n+this.p<0){return;}
+            if(n==0||n+this.pt<0){return;}
             var bz=n<0?n:0, fz=n<0?0:n;
             this.changed=true;
-            this.a=this.a.slice(0,this.p+bz)+this.a.slice(this.p+fz);
+            this.a=this.a.slice(0,this.pt+bz)+this.a.slice(this.pt+fz);
             this.mov(bz);
-            this.change=[this.p,this.p+n];
+            this.change=[this.pt,this.pt+n];
         },
         mov(n=1){// move the cursor
-            this.oldp=this.p;//previous cursor position
-            this.p=this.p+n;
-            if(this.p<0){this.p=0;}
-            else if(this.p>this.a.length){this.p=this.a.length;}
+            this.pt=this.pt+n;
+            if(this.pt<0){this.pt=0;}
+            else if(this.pt>this.a.length){this.pt=this.a.length;}
         },
     }),
     buf=Buffer();
@@ -67,29 +70,21 @@ var update=(rks)=>{
     }
 };
 
-var spot={x:20,y:0,h:0};
+var spot={x:20,y:0,h:0,cached:false};// screen border, cached offsets
 spot.y=spot.h+spot.x;
 
-var render_cursor=(t)=>{
-    // where is the cursor on the screen?
-    var clr=Math.abs(Math.cos(t/500));
-    var h=p.measureText('W').width;
-    var w=p.measureText(buf.a.slice(0,buf.p)).width;//ideally slice(buf.current_line_p,buf.p)
-    //p.clearRect(0,0,p.canvas.width,p.canvas.height);//whole canvas?!
-    //var oldw=p.measureText(buf.a.slice(buf.oldp,Math.abs(buf.p+buf.oldp))).width;
-    var pdiff=buf.p-buf.oldp;// left==negative
-    var oldw=(p.measureText(buf.a.slice(0,buf.oldp).width))-(p.measureText(buf.a.slice(0,buf.p)).width);
-    p.fillStyle='rgba(150,150,150,0.1)';
-    p.fillRect(spot.x+oldw-10,spot.y-spot.h,12,spot.h);
+var render_cursor=()=>{
+    var h=p.measureText('W').width,
+        curln=buf.lines().length-1,
+        lastnl=buf.a.lastIndexOf('\n',buf.pt)+1,//+1 handles 'no newlines' special case
+        w=p.measureText(buf.a.slice(lastnl,buf.pt)).width,
+        clr=Math.abs(Math.cos(performance.now()/500));
+    p.clearRect(0,0,p.canvas.width,p.canvas.height);//whole canvas?!
     p.fillStyle='rgba(20,255,255,'+clr+')';
-    p.fillRect(spot.x+w,spot.y-spot.h,1,spot.h);
+    p.fillRect(spot.x+w,spot.y+h*curln-spot.h,1,spot.h);
 };
 
 var render_text=()=>{
-    // in response to a change in screen size or text
-    // the portion of text drawn should be a function of:
-    // 1. cursor position (n lines above, m below)
-    // 2. screen area, font size, word wrapping
     spot.h=c.measureText('W').width;
     c.clearRect(0,0,c.canvas.width,c.canvas.height);
     spot.y=spot.h+20;// border-top
@@ -98,35 +93,32 @@ var render_text=()=>{
 };
 
 var gameloop=(now,resiz)=>{
-    //requestAnimationFrame((now)=>gameloop(now,false));
-    render_cursor(now);
     update(KEYQ);
     if(buf.changed||resiz){
         render_text();
         buf.changed=false;
     }
 };
-//requestAnimationFrame((now)=>gameloop(now,true));
 
 var rsz=()=>{
-    c.canvas.width=c.canvas.clientWidth;
-    c.canvas.height=c.canvas.clientHeight;
+    p.canvas.width=c.canvas.width=c.canvas.clientWidth;
+    p.canvas.height=c.canvas.height=c.canvas.clientHeight;
+    p.font=c.font='24px Sans-Serif';
     c.fillStyle='#cacada';
-    c.font='24px Sans-Serif';
-    p.canvas.width=p.canvas.clientWidth;
-    p.canvas.height=p.canvas.clientHeight;
-    p.font='24px Sans-Serif';
     requestAnimationFrame((now)=>gameloop(now,true));
 };
 
-window.onload=rsz;
+window.onload=()=>{
+    rsz();
+    setInterval(render_cursor,1000/20);
+}
 window.onresize=rsz;
 window.onkeydown=(k)=>{
     if(k.type=='keydown'){// push incoming events to a queue as they occur
         k.preventDefault();
         KEYQ.push({mods:[k.altKey,k.ctrlKey,k.metaKey,k.shiftKey], k:k.code});
+        requestAnimationFrame((now)=>gameloop(now,true));
     }
 };
 
 buf.load('a test without a newline');
-
