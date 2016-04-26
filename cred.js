d8989f6b24d1577b604075ec709b76d905fded50
hoosierEE <ashroyer@gmail.com>
2016-04-25 22:21:13 -0400

    rearranging cursor and text drawing

--- cred.js
+++ cred.js
@@ -5,19 +5,20 @@ var c=document.getElementById('c').getContext('2d'),// rarely changing bottom ca
     MODE='normal',// Vim modes: normal, insert, visual[-block, -line], etc.
     ESC_FD=0,// 'fd' escape sequence
     KEYQ=[{mods:[false,false,false,false],k:''}],// lightens duties for key event handler
+
     Buffer=()=>({// a string with a cursor
         pt:0,// number of chars between start of file and cursor position
-        ln:0,// line containing pt
+        ln:[0,0],// [previous line, current line]
         co:0,// column containing pt
         a:'',// text buffer
         txt_changed:false,
-        change:[0,0],// [previous_pt, current_pt]
-        load(txt){this.a='';this.change=[0,0];this.pt=0;this.ins(txt);this.mov(0);},
+        pos:[0,0],// [previous, current_pt]
+        load(txt){this.a='';this.pos=[0,0];this.pt=0;this.ins(txt);this.mov(0);},
         lines(){return this.a.split(/\n/g);},// array of all the buffer's lines
         words(){return this.a.split(/\s/g).reduce((a,b)=>a.concat(b),[]);},
         // modes
         append_mode(){if(this.a[this.pt]!=='\n'){this.mov(1);}},
-        insert_mode(){/* a no-op, to make the API feel more natural */},
+        insert_mode(){/* a no-op, to make append_mode less lonely */},
         esc_fd(){
             this.del(-2);
         },
@@ -37,68 +38,85 @@ var c=document.getElementById('c').getContext('2d'),// rarely changing bottom ca
             this.txt_changed=true;
         },
         mov(n=1){// move cursor
-            this.change[0]=this.pt;// location before move
+            this.pos[0]=this.pt;// location before move
             //this.pt=this.pt+n;
             if(this.pt+n<0){this.pt=0;}
             else if(this.pt+n>this.a.length){this.pt=this.a.length;}
             else{this.pt+=n;}
-            this.change[1]=this.pt;// location after move
+            this.pos[1]=this.pt;// location after move
             // update line number
-            for(var i=this.change[n<0?1:0];i<this.change[n<0?0:1];++i){
-                if(this.a[i]&&this.a[i]=='\n'){this.ln+=(n<0?-1:1);if(this.ln<0){this.ln=0;}}
+            for(var i=this.pos[n<0?1:0];i<this.pos[n<0?0:1];++i){
+                if(this.a[i]&&this.a[i]=='\n'){
+                    this.ln[0]=this.ln[1];// save previous line
+                    this.ln[1]+=(n<0?-1:1);// update current line
+                    if(this.ln[1]<0){this.ln[1]=0;}
+                }
             }
         },
     }),
+
     ScreenOffsets=()=>({
-        x:20,y:20,h:0,lh:undefined,
-        init(ctx){this.lh=ctx.measureText('W').width;}
+        x:20,y:20,// border width
+        h:0,
+        lmul(lnum){
+            return this.y+this.lh*lnum-this.h;
+        },
+        lh:0,
+        init(ctx){
+            this.h=c.measureText('W').width;
+            this.lh=ctx.measureText('W').width;
+        }
     }),
     buf=Buffer(),
     offs=ScreenOffsets();
 
 var render_cursor=()=>{
+    // re-render the text between where the cursor is now, and where it was last,
+    // then render the cursor itself
 
-    // left edge of cursor, currently
-    var cur_l=p.measureText(buf.a.slice(buf.a.lastIndexOf('\n',buf.change[1]-1)+1,buf.change[1])).width;
-    // right edge of cursor, currently
-    var cur_r,wid;
+    // current cursor info (x offset, char underneath it, width of char)
+    var cur_x=p.measureText(buf.a.slice(buf.a.lastIndexOf('\n',buf.pos[1]-1)+1,buf.pos[1])).width;
+    var cur_char=buf.a[buf.pos[1]]||'';// char under the cursor, now
+    var cur_wid; // right edge of cursor, now
 
     // left edge of cursor, previously
-    var prev_l=p.measureText(buf.a.slice(buf.a.lastIndexOf('\n',buf.change[0]-1)+1,buf.change[0])).width;
-    // right edge of cursor, previously
-    var prev_r,pwid;
+    var prev_x=p.measureText(buf.a.slice(buf.a.lastIndexOf('\n',buf.pos[0]-1)+1,buf.pos[0])).width;
+    var prev_char=buf.a[buf.pos[0]]||'';// char under the cursor, previously
+    var prev_wid; // right edge of cursor, previously
+
+    // x and y offsets (width and height of cursor)
+    var ofx=offs.x+cur_x;
+    var ofy=offs.lmul(buf.ln[1]);
+    var prev_ofx=offs.x+prev_x;
+    var prev_ofy=offs.lmul(buf.ln[0]);
+
+    var sortedpos=buf.pos.sort();
+    var text_between=buf.a.slice(sortedpos[0],sortedpos[1]+1);// from old pos to current
+    console.log(text_between);
 
     p.clearRect(0,0,p.canvas.width,p.canvas.height);//whole canvas?!
-    if(MODE=='normal'){
-        cur_r=buf.a[buf.change[1]];
-        prev_r=buf.a[buf.change[0]];
-        p.fillStyle='orange';
-        console.log(cur_r+' '+prev_r);
-        if(cur_r=='\n'||cur_r==undefined){wid=10;}
-        else{
-            p.save();
-            // draw the character under the cursor
-            p.fillStyle='black';
-            p.fillText(buf.a[buf.change[1]],offs.x+cur_l,(offs.y+offs.lh*buf.ln));
-            // draw the character at the previous position
-            p.fillStyle='magenta';
-            p.fillText(buf.a[buf.change[0]],offs.x+prev_l,(offs.y+offs.lh*buf.ln));
-            p.restore();
-            wid=p.measureText(cur_r).width;
-        }
-    }else{
-        wid=1;// thin cursor during insert operation
+    if(MODE=='insert'){
+        cur_wid=1;// thin cursor during insert operation
         p.fillStyle='lime';
+    }else{
+        p.fillStyle='orange';
+        if(cur_char=='\n'||cur_char==''){cur_wid=10;}
+        else{
+            cur_wid=p.measureText(cur_char).width;
+            var curln=offs.y+offs.lh*buf.ln[1];
+
+            p.save();
+            p.fillStyle='gray';
+            p.fillText(cur_char,offs.x+cur_x,curln); // draw the character under the cursor
+            p.fillStyle='magenta';
+            p.fillText(prev_char,offs.x+prev_x,curln); // draw the character at the previous position
+            p.restore();
+        }
     }
-    var ofx=offs.x+cur_l;
-    var ofy=offs.y+offs.lh*(buf.ln)-offs.h;
-    var cwd=wid;
-    var cwh=offs.h*1.1;
-    p.fillRect(ofx,ofy,cwd,cwh);
+    p.fillRect(ofx,ofy,cur_wid,offs.h);
 };
 
 var render_text=()=>{
-    offs.h=c.measureText('W').width;
     c.clearRect(0,0,c.canvas.width,c.canvas.height);
     offs.y=offs.h+20;// border-top
     buf.lines().forEach((l,i)=>c.fillText(l,offs.x,offs.y+(i*offs.h)));
