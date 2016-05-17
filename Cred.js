b2822c2a8f272d83c031e968abc2fd1db1d6799f
hoosierEE <ashroyer@gmail.com>
2016-05-16 20:58:23 -0400

    minor rearrangement

--- Cred.js
+++ Cred.js
@@ -3,8 +3,58 @@
 var c=document.getElementById('c').getContext('2d'),
     KEYQ=[{mods:[false,false,false,false],k:''}],// lightens duties for key event handler
 
-    ////
-    Buffer=()=>({
+
+    Window=(c)=>({// class @param c: the target Canvas
+        // STATE
+        bw:20,// border width
+        // defaults should be bad enough to clue you in that something's awry
+        line_height:10, line_descent:5, v:{},// viewport position and size
+
+        // METHODS
+        ln_top(n){return this.bw+this.line_height*(n+1);},// top pixel of line n
+        co_left(n){return this.bw+c.measureText(buf.getline(cur.cl).slice(0,n)).width;},// left edge of column n
+        co_right(n){return this.bw+c.measureText(buf.getline(cur.cl).slice(0,n+1)).width;},// right edge of column n
+        num_visible_lines(){return (c.canvas.height-2*this.bw)/this.line_height|0;},
+
+        scroll(soff=5){
+            if(soff>this.num_visible_lines()){soff=this.num_visible_lines()/2|0;}
+            else if(soff<1){soff=1;}// smallest usable value - 0 is too small
+            var prev_y=this.v.y, prev_x=this.v.x;// grab current value of x and y
+
+            // scroll up or down
+            var ltop=this.ln_top(cur.cl+soff), lbot=this.ln_top(cur.cl-soff);
+            if(ltop>this.v.y+this.v.h){this.v.y+=ltop-(this.v.y+this.v.h);}
+            if(lbot<this.v.y){this.v.y-=this.v.y-lbot;}
+
+            // scroll left or right
+            var crt=this.co_right(cur.co)+this.bw, clt=this.co_left(cur.co)-this.bw;
+            if(crt>this.v.x+this.v.w){this.v.x+=crt-this.v.w;}
+            if(clt<this.v.x){this.v.x-=this.v.x-clt;}
+
+            if(this.v.y<0){this.v.y=0;}
+            if(this.v.x<0){this.v.x=0;}
+            // move canvas opposite of viewport if x or y changed
+            if(prev_x!=this.v.x||prev_y!=this.v.y){c.setTransform(1,0,0,1,-this.v.x,-this.v.y);}
+        },
+        init(ctx){// must be called before using other Window methods
+            var fm=FontMetric(cfg.font_name,cfg.font_size);
+            this.line_ascent=fm[0];// top of text such as QMEW|
+            this.line_height=fm[1];// total line height
+            this.line_descent=fm[2];// lower bound of text such as: jgpq|
+            this.v={x:0,y:0,w:c.canvas.width,h:c.canvas.height};
+            this.scroll();
+        },
+    }),
+
+
+    Configuration=()=>({// class
+        font_size:'20px',
+        font_name:'courier new',//'Sans-Serif',
+        init(c){c.font=this.font_size+' '+this.font_name; c.fillStyle='#dacaba';},
+    }),
+
+
+    Buffer=()=>({// class
         // STATE
         s:'',
         pt:0,
@@ -38,8 +88,7 @@ var c=document.getElementById('c').getContext('2d'),
     }),
 
 
-    ////
-    Cursor=(b)=>({
+    Cursor=(b)=>({// class
         /* Cursor
            Given a Buffer b:
            * keep track of editing "mode" (normal, insert, etc.)
@@ -139,69 +188,16 @@ var c=document.getElementById('c').getContext('2d'),
     }),
 
 
-    ////
-    Parser=()=>({
+    Parser=()=>({// class
         fsm:{mul:'',verb:'',mod:'',state:''},
-        parse(dec){
+        parse(dec){// parse : Decoder -> Action
             if(dec.code.search(/\d/)!==-1){this.fsm.mul+=dec.code;}
             if(dec.code.search(/]fFtT]/)!==-1){this.fsm.verb='find';}
             console.log(this.fsm);
         },
     }),
 
-
-    ////
-    Configuration=()=>({
-        font_size:'20px',
-        font_name:'courier new',//'Sans-Serif',
-        init(c){c.font=this.font_size+' '+this.font_name; c.fillStyle='#dacaba';},
-    }),
-
-
-    ////
-    Window=(c)=>({// c: the target Canvas
-        // STATE
-        bw:20,// border width
-        // defaults should be bad enough to clue you in that something's awry
-        line_height:10, line_descent:5, v:{},// viewport position and size
-
-        // METHODS
-        ln_top(n){return this.bw+this.line_height*(n+1);},// top pixel of line n
-        co_left(n){return this.bw+c.measureText(buf.getline(cur.cl).slice(0,n)).width;},// left edge of column n
-        co_right(n){return this.bw+c.measureText(buf.getline(cur.cl).slice(0,n+1)).width;},// right edge of column n
-        num_visible_lines(){return (c.canvas.height-2*this.bw)/this.line_height|0;},
-
-        scroll(soff=5){
-            if(soff>this.num_visible_lines()){soff=this.num_visible_lines()/2|0;}
-            else if(soff<1){soff=1;}// smallest usable value - 0 is too small
-            var prev_y=this.v.y, prev_x=this.v.x;// grab current value of x and y
-
-            // scroll up or down
-            var ltop=this.ln_top(cur.cl+soff), lbot=this.ln_top(cur.cl-soff);
-            if(ltop>this.v.y+this.v.h){this.v.y+=ltop-(this.v.y+this.v.h);}
-            if(lbot<this.v.y){this.v.y-=this.v.y-lbot;}
-
-            // scroll left or right
-            var crt=this.co_right(cur.co)+this.bw, clt=this.co_left(cur.co)-this.bw;
-            if(crt>this.v.x+this.v.w){this.v.x+=crt-this.v.w;}
-            if(clt<this.v.x){this.v.x-=this.v.x-clt;}
-
-            if(this.v.y<0){this.v.y=0;}
-            if(this.v.x<0){this.v.x=0;}
-            // move canvas opposite of viewport if x or y changed
-            if(prev_x!=this.v.x||prev_y!=this.v.y){c.setTransform(1,0,0,1,-this.v.x,-this.v.y);}
-        },
-        init(ctx){// must be called before using other Window methods
-            var fm=FontMetric(cfg.font_name,cfg.font_size);
-            this.line_ascent=fm[0];// top of text such as QMEW|
-            this.line_height=fm[1];// total line height
-            this.line_descent=fm[2];// lower bound of text such as: jgpq|
-            this.v={x:0,y:0,w:c.canvas.width,h:c.canvas.height};
-            this.scroll();
-        },
-    }),
-
-    //// instances of the above classes
+    // instances of the above classes
     cfg=Configuration(),
     buf=Buffer(),
     cur=Cursor(buf),
@@ -209,8 +205,8 @@ var c=document.getElementById('c').getContext('2d'),
     win=Window(c);
 
 // decode : RawKey -> DecodedKey
-var decode=(rk)=>{
-    var k=rk.k, mods=rk.mods;// get the components of the RawKey object
+var decode=({k, mods})=>{
+    //var k=rk.k, mods=rk.mods;// get the components of the RawKey object
     var dec={type:'',code:'',mods:mods};// return type (modifiers pass through)
     // printable
     if(k==='Space'){dec.code=' ';}
