64892aab5ba7aef57f45a5e0dce03c9fa401cd72
ashroyer <ashroyer@gmail.com>
2016-06-06 17:10:56 -0400

    tree of tokens

--- Parser.js
+++ Parser.js
@@ -5,7 +5,7 @@
    e.g: 5d2w = (repeat-five-times (delete (repeat-two-times (word-forward))))
 */
 var Parser=(cur)=>{/* Convert keyboard events into Actions */
-    var ParserCommand=()=>({c:'',p:''}),// current, previous
+    var ParserCommand=()=>({c:'',p:''}),
 
         arrow=(dec)=>{
             switch(dec.code){
@@ -22,9 +22,8 @@ var Parser=(cur)=>{/* Convert keyboard events into Actions */
             switch(dec.type){
             case'print':
                 cur.ins(dec.code);cur.rowcol();
-                // auxiliary escape methods: quickly type 'fd', or use the chord 'C-['
-                if(dec.code==='f'){fd=-t;}if(dec.code==='d'&&fd<0&&t+fd<500){cur.esc(2);}
-                if(dec.code==='['&&dec.mods[1]){cur.esc(1);}
+                if(dec.code==='f'){fd=-t;}if(dec.code==='d'&&fd<0&&t+fd<500){cur.esc(2);}// 'fd'
+                if(dec.code==='['&&dec.mods[1]){cur.esc(1);}// 'C-['
                 break;
             case'edit':
                 if(dec.code==='B'){cur.del_backward();}// backspace
@@ -35,44 +34,60 @@ var Parser=(cur)=>{/* Convert keyboard events into Actions */
             }
         },
 
-        single_token=(dec)=>{
-            switch(dec.code){
+        single_token=(code)=>{
+            var result=true;
+            switch(code){
                 // motions
             case'j':cur.down(1);break;
             case'k':cur.up(1);break;
             case'l':cur.right(1);break;
             case'h':cur.left(1);break;
-            case'0':cur.to_bol();break;
             case'$':cur.to_eol();break;
+            case'0':cur.to_bol();break;
             case'{':cur.backward_paragraph();break;
             case'}':cur.forward_paragraph();break;
             case'G':cur.to_eob();break;
             case'b':cur.backward_word();break;
             case'e':cur.forward_word();break;
-                // mode changers
+                // editing actions
             case'i':cur.insert_mode();break;
             case'a':cur.append_mode();break;
-                // editing actions
             case'x':cur.del_at_point();break;
             case'D':cur.del_to_eol();break;
-            default:break;
+                // leader key
+            case' ':console.log('SPC-');break;// TODO SPC-prefixed functions a-la Spacemacs!
+            default:result=false;break;
             }
-            //case' ':console.log('SPC-');break;// TODO SPC-prefixed functions a-la Spacemacs!
+            return result;
         },
 
-        app=(d,c)=>{if(!(d.mods[0]||d.mods[1]||d.mods[2])){c.c+=d.code;}},// append non-shifted code
+        append_char=(d,c)=>{if(!(d.mods[0]||d.mods[1]||d.mods[2])){c.c+=d.code;}},// append non-shifted code
 
-        /* command tokenizers */
+        rxall=(regex,str)=>{
+            var arr,result=[];
+            while((arr=regex.exec(str))!==null){result.push({val:arr[0],idx:arr.index});}
+            if(!result.length){result=[{val:1,idx:-1}];}
+            return result;
+        },
+
         modifier=/a|i/,
-        motion=/[beGhjklw$0^]/,
+        motion=/[beGhjklw$^]|gg/,
         multiplier=/[1-9][0-9]*/g,
         object=/[wWps(){}\[\]"'`]/,
         operator=/[cdy]/,
-        tkzs=[modifier,motion,multiplier,object,operator],
 
-        tokenize=(cmd,ismotion)=>{
-            var t={a:tkzs.map(t=>cmd.match(t))};
-            t.times=cmd.match(multiplier)||[1];
+        /* get the tokens, not necessarily in order */
+        tokenize=(cmd)=>{
+            var rx=(r,s)=>{var res=r.exec(s); return res?{val:res,idx:res.index}:{val:[-1],idx:-1};},
+                idsort=(x,y)=>x.idx>y.idx?1:((x.idx<y.idx)?-1:0),
+                res=[modifier,motion,object,operator].map(x=>rx(x,cmd))
+                .concat(rxall(multiplier,cmd))
+                .filter(x=>x.idx>=0).sort(idsort);
+
+            var t={
+                original:cmd,// for debugging
+                tree:res,
+            };
             return t;
         };
 
@@ -82,30 +97,20 @@ var Parser=(cur)=>{/* Convert keyboard events into Actions */
             if(dec.type==='arrow'){arrow(dec);}// parse arrows in any mode
             if(cur.mode==='insert'){insert(t,dec);}
             else{
-                app(dec,this.cmd);// build the command 1 char at a time
-                //console.log(this.cmd);
-
-                // if the command ends in a text object or motion, parse the command
-                if(this.cmd.c==='i'){cur.insert_mode(); this.cmd={c:'',p:this.cmd.c};}
-                else if(this.cmd.c==='a'){cur.append_mode(); this.cmd={c:'',p:this.cmd.c};
-                }
-                else{
-                    var [mo,ob]=[motion,object].map(a=>!!(this.cmd.c.match(a)));
-                    if(mo||ob){
-
+                append_char(dec,this.cmd);// build the command 1 char at a time
+                if(single_token(this.cmd.c)){this.cmd.c='';}
+                else{/* if the command ends with a text object or motion, parse the command */
+                    if([motion,object].some(x=>x.test(this.cmd.c))){
                         // tokenize
-                        var tokens=tokenize(this.cmd.c,mo);
-                        console.log(JSON.stringify(tokens));
+                        var tokens=tokenize(this.cmd.c);
+                        console.log(JSON.stringify(tokens,null,4));
 
                         // parse the command, consume the cmd string
-                        if(tokens.times.length<2){
-                            for(var i=0,l=tokens.times.pop(),i<l;++i){
-                                single_token(dec);
-                            }
+                        var times=tokens.multiplier?tokens.multiplier.pop().val:1;
+                        for(var i=0;i<times;++i){
+                            single_token(dec.code);
                         }
-
-                        // clean up
-                        this.cmd={c:'',p:this.cmd.c};
+                        this.cmd={c:'',p:this.cmd.c};// keep last command in history, clear current one
                     }
                 }
             }
